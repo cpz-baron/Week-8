55615c38f7bd8727477094a96af10c61970b8480
cpz-baron <57302955+cpz-baron@users.noreply.github.com>
2020-02-28 22:57:09 -0800

    Add files via upload

--- app.R
+++ app.R
@@ -1,40 +1,15 @@
-# Exercise 2: a basic Shiny app
+# Exercise 4: complex Shiny UI layouts
 
-# Load the `shiny` package
+# Load libraries so they are available
+library("shiny")
 
+# Use source() to execute the `app_ui.R` and `app_server.R` files. These will
+# define the UI value and server function respectively.
+source("app_ui.R")
+source("app_server.R")
 
-# Define a new `ui` variable. This variable should be assigned a `fluidPage()` layout
-# The `fluidPage()` layout should be passed the following:
+# You will need to fill in the `app_ui.R` file to create the layout.
+# Run the app through this file.
 
-  
-  # A `titlePanel()` layout with the text "Cost Calculator"
-
-  
-  # A `numericInput()` widget with the label "Price (in dollars)"
-  # It should have a default value of 0 and a minimum value of 0
-  # Hint: look up the function's arguments in the documentation!
-
-  
-  # A second `numericInput()` widget with the label "Quantity"
-  # It should have a default value of 1 and a minimum value of 1  
-
-  
-  # The word "Cost", strongly bolded
-
-  
-  # A `textOutput()` output of a calculated value labeled `cost`
-
-
-
-# Define a `server` function (with appropriate arguments)
-# This function should perform the following:
-
-  
-  # Assign a reactive `renderText()` function to the output's `cost` value
-  # The reactive expression should return the input `price` times the `quantity`
-  # So it looks nice, paste a "$" in front of it!
-
-
-
-# Create a new `shinyApp()` using the above ui and server
-
+# Create a new `shinyApp()` using the loaded `ui` and `server` variables
+shinyApp(ui = ui, server = server)
